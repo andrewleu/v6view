72d598813f1166c162a8c82179b7192d4155c30f
root <root@localhost.localdomain>
2019-06-03 14:57:23 +0800

    probing site

--- probe_nodes.py
+++ probe_nodes.py
@@ -9,110 +9,36 @@ import select
 import os
 import urllib2
 
-class updatedb (threading.Thread):
-    def connect_remote(self, cmmd):
-        req = urllib2.Request(cmmd)
-        try:
-            reslt = urllib2.urlopen(req, data=None, timeout=15)
-        except:
-            return '500'
-        return  reslt.getcode()
-    def run(self) :
-       dbaddr = "127.0.0.1"
-       conn=mysql.connect(dbaddr,'ipv6','ipv6bgp','v6view')
-       cur=conn.cursor();
-       mutex.acquire()
-       try :
-         if not cur.execute("select id from v6view where regstat='1'") :
-             cur.execute("update v6view set regstat='1'")
-       except Exception as e :
-             print e 
-       finally :
-          mutex.release()    
-       while 1 :
-           mutex.acquire()
-           if  not cur.execute("select id, ipv4, ipv6, httpstat from v6view where regstat='1' order by rand() limit 1") :
-                 mutex.release()
-                 break
-           item=cur.fetchone(); err=""
-           try :
-                cur.execute("update v6view set regstat='0' where id=%d" % item[0])
-                cur.execute("commit")
-           except Exception as err :
-                print err
-           finally :
-                mutex.release()
-           if err !='' :
-                continue
-           addr=item[1]
-           cmd = 'http://' + addr + ':9050/'+'site/domainARecord?website=www.qq.com'
-           result=self.connect_remote(cmd); print "%s, addr=%s,  %s" %  (result, addr, item[0])
-           cur.execute("update v6view set httpstat='%s'where id='%s'" % (result,item[0]))
-           cur.execute("commit")
-       print self 
-       cur.close()
-       conn.close()
-       return 1
-
-
-global mutex
+global mutex, nodes,version
 mutex=threading.Lock()
-n=8
-threads=[]
-startTime=int(time.time())
-for thrd in range(n) :
-    try :
-	th=updatedb()
-        print th
-        threads.append(th)
-	th.start()
-    except:
-       sleep(10)
-#   del rfile
-       print "Exception occurs"
-for th in threads :
-   try :
-      print th
-      th.join()
-      time.sleep(5)
-   finally :
-      print "end" 
-endTime=int(time.time())
-print "Duration %s"  % (endTime-startTime)
-exit(0)
-
-
-"""
-
-def connect_remote(cmmd):
+class updatedb(threading.Thread) :
+  def connect_remote(self, cmmd):
     req = urllib2.Request(cmmd)
     try:
         reslt = urllib2.urlopen(req, data=None, timeout=15)
         return reslt.read()
     except Exception as e:
         return e
-    
-
-
-version='4'
-reload(sys)
-sys.setdefaultencoding("utf8")
-dbaddr = "127.0.0.1"
-tabl = mysql.connect(dbaddr, 'root', 'rtnet', 'v6view', charset='utf8')
-cur_tab = tabl.cursor();
+  def run(self) :
+      sys.setdefaultencoding("utf8")
+      dbaddr = "127.0.0.1"
+      tabl_sub = mysql.connect(dbaddr, 'root', 'rtnet', 'v6view', charset='utf8')
+      cur_tab_sub = tabl_sub.cursor();
 #fp=open("log",'wb')
-
-cur_tab.execute("set names 'utf8'");  # be sure to encode in utf8
-# tabl=sqlite3.connect('v6view.s3db',check_same_thread = False)
-# tabl.text_factory = lambda x: unicode(x, "utf-8", "ignore")
-# cur_tab=tabl.cursor()
-#cur_tab.execute("select url, type from urllist");urls = cur_tab.fetchall()
-cur_tab.execute("select id, name,  ipv4, ipv6 from v6view")
-nodes= cur_tab.fetchall()
-cur_tab.execute("truncate nodestatus")
-# fetch all the url in the list
-# 如果表里所有Regstat为1，说明已经跑完，重新把?regstat设为0，重新进行测试
-for  node in nodes :
+      cur_tab_sub.execute("set names 'utf8'");  #
+      while 1 :
+        error=''
+        mutex.acquire() 
+        try :
+              node = nodes[-1]
+              del nodes[-1] ;# cur the tail
+        except Exception as e :
+              error= e 
+              print e 
+        finally :
+             mutex.release()
+        if error :
+           break
         url = "www.qq.com";i=0
         if version == '4':
                 para1 = node[2];  # source ipv4addr
@@ -126,14 +52,55 @@ for  node in nodes :
         else:
                 cmd = 'http://' + para1 + ':9050/';  # /site/domainARecord?website=+para2
         req = cmd + 'site/homePage?website=' + para2 + '&status=0&ipstatus=4'
-        result=connect_remote(req)
-        
-        print result; #fp.write(str(result)+'\r')
-        cur_tab.execute("insert nodestatus(id, result) value(%d,'%s' )" % (node[0], result)) 
-        print " "
-        cur_tab.execute("commit")
-fp.close()		
+        result=self.connect_remote(req)
+        print "return result %s" %  result; #fp.write(str(result)+'\r')
+        cur_tab_sub.execute("insert nodestatus(id, result) value(%d,'%s' )" % (node[0], result)) 
+        cur_tab_sub.execute("commit")            
+      cur_tab_sub.close()
+      tabl_sub.close()
+
+
+version='4'
+reload(sys)
+sys.setdefaultencoding("utf8")
+dbaddr = "127.0.0.1"
+tabl = mysql.connect(dbaddr, 'root', 'rtnet', 'v6view', charset='utf8')
+cur_tab = tabl.cursor();
+#fp=open("log",'wb')
+cur_tab.execute("set names 'utf8'");  # be sure to encode in utf8
+# tabl=sqlite3.connect('v6view.s3db',check_same_thread = False)
+# tabl.text_factory = lambda x: unicode(x, "utf-8", "ignore")
+# cur_tab=tabl.cursor()
+#cur_tab.execute("select url, type from urllist");urls = cur_tab.fetchall()
+cur_tab.execute("select id, name,  ipv4, ipv6 from v6view")
+nodes= list(cur_tab.fetchall())
+cur_tab.execute("truncate nodestatus")
+n=2
+threads=[]
+startTime=int(time.time())
+for thrd in range(n) :
+    try :
+      	th=updatedb()
+        print th
+        threads.append(th)
+        th.start()
+    except Exception as e :
+       sleep(10)
+#   del rfile
+       print e
+for th in threads :
+   try :
+      print th
+      th.join()
+      time.sleep(5)
+   finally :
+      print "end" 
+endTime=int(time.time())
+print "Duration %s"  % (endTime-startTime)
+cur_tab.execute("update v6view set httpstat='500' where httpstat='200'")
+cur_tab.execute("commit")
+cur_tab.execute("update v6view, nodestatus set v6view.httpstat='200' where nodestatus.id=v6view.id and nodestatus.result regexp '^[1-9][0-9]*$'")
+cur_tab.execute("commit")
 cur_tab.close()
 tabl.close()
 
-"""
