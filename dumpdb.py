f57efc8b303f2261e0c548e47478ca496198d479
andrewleu <andrewleu@hotmail.com>
2019-09-26 12:45:59 +0800

    dumpdb.py updating

--- dumpdb.py
+++ dumpdb.py
@@ -25,26 +25,35 @@ dbaddr=config.get("dbAddr")
 username=config.get("rootName")
 userpass=config.get("rootPass")
 tabl = mysql.connect(dbaddr,username ,userpass , 'v6view', charset='utf8')
+#read configuration from a json file
 cur_tab = tabl.cursor();
 cur_tab.execute("set names 'utf8'");  # be sure to encode in utf8
+cur_tab.execute("select date_sub(curdate(),INTERVAL WEEKDAY(curdate()) + 0 DAY)")
+file_daystr=cur_tab.fetchone()[0];
+#the date of monday ofr this week
+start_time=file_daystr.isoformat()+' '+'00:00:00'
 try :
    e=''
    child =subprocess.Popen("rm -f /var/lib/mysql-files/dbdumping.csv",shell=True)
    #remove previous dump file
    child.wait()
-   cur_tab.execute("select * from (select 'id', 'ori','ori_ipv6','des','des_ipv6','date' ,'code','ipv4delay',\
-   'ipv4doudong','ipv4lost','ipv6delay','ipv6doudong','ipv6lost' union select id,ori,ori_ipv6,des,des_ipv6,\
+   cur_tab.execute("select min(id) from test6 where date> '%s'" % start_time)
+   minid=cur_tab.fetchone()
+   #the lowest id of the entry of this week
+   cur_tab.execute("select * from (select 'id', 'ori','ori_ipv4','ori_ipv6','des','des_ipv4','des_ipv6','date' ,'code','ipv4delay',\
+   'ipv4doudong','ipv4lost','ipv6delay','ipv6doudong','ipv6lost' union select id,ori,ori_ipv4,ori_ipv6,des,des_ipv4,des_ipv6,\
    date, code,ip4delay, ipv4doudong, ipv4lost,ipv6delay,ipv6doudong,ipv6lost from \
-   nodes_ping_each) tab into outfile '/var/lib/mysql-files/dbdumping.csv' \
-   fields terminated by ',' ")
-
+   nodes_ping_each where id>%d) tab into outfile '/var/lib/mysql-files/dbdumping.csv' \
+   fields terminated by ',' " % minid); #dumping
 except Exception as e :
    print e
 finally :
    cur_tab.close()
    tabl.close()
 if e=='' :
-   #child=subprocess.Popen("cd /var/lib/mysql-files",shell=True)
-   #child.wait()
-   subprocess.Popen("zip  -j /opt/lampp/htdocs/pma/dbd.zip /var/lib/mysql-files/dbdumping.csv",shell=True)
+   print datetime.datetime.now().isoformat()
+   command_str="zip  -j /opt/lampp/htdocs/pma/dbd"+file_daystr.isoformat().replace("-",'')+".zip"\
+   +' '+'/var/lib/mysql-files/dbdumping.csv'
+   subprocess.Popen(command_str,shell=True).wait()
    # -j to remove path information
+
