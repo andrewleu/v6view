8d6fd7d12377188ce2f9866499fff9a220f38ae3
andrewleu <andrewleu@hotmail.com>
2020-01-08 12:06:00 +0800

    dump kv to a file

---
+++ csv_to_kv.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+# to dump a table to file
+import datetime
+import json
+import sys
+import urllib2
+import MySQLdb as mysql
+from sys import argv
+import subprocess
+from pymongo import MongoClient as mongodb
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+if len(argv) < 3:
+    print  argv
+    print "Usage: %s dbname filename" %argv[0]
+    exit(0)
+else:
+    dbname=argv[1]
+    csvfile=argv[2]
+
+
+try :
+   e=''
+   fh=open('dbconfig.json',mode='r');#file handler
+   config=fh.read()
+except Exception as e:
+   print e
+finally:
+   fh.close()
+
+if e!='' :
+   exit()
+#resolve json file to abstract parameter
+config=json.loads(config)
+config=config.get("mongodb")
+dbaddr=config.get("Addr")
+username=config.get("User")
+userpass=config.get("Pass")
+client=mongodb(dbaddr, 27017)
+db=client.admin
+db.authenticate(username,userpass)
+db=client[dbname]
+col=db[dbname]
+try:
+  fh=open(csvfile,mode='r')
+  dpfh=open("dumping",mode='w')
+  i=1 
+  head=fh.readline()
+  head=head.strip().split(',')
+  while 1 :
+    line=fh.readline()
+    if line=='' :
+        break
+    line=line.decode().strip().split(',')
+    doc=zip(head,line) ; # make key value tuple pair
+    doc=dict(doc)      ; # make key value document    
+    doc["index"]=i
+    print doc
+    dpfh.write(str(i))
+    dpfh.write('\n')
+    for k,v in doc.items() : 
+    # read key value from the dict
+       dpfh.write(str(k).encode('utf-8')); #中文写入
+       dpfh.write(': ')
+       dpfh.write(str(v).encode('utf-8'))
+       dpfh.write('\n')
+  #  col.insert(doc)   ;# print i; # insert into db
+    i+=1
+except Exception as e :
+    #pinrt "lindde: %s ", % i
+    print e
+finally:
+    fh.close()
+    dpfh.close()
+    client.close()
+
